30d8c736b2336c9723ee786f2554cfeee7e995ed
M4TH3US17 <matheusdalvino50@gmail.com>
2022-08-06 09:07:58 -0400

    Filtro por modelo

--- src/services/ListagemService.js
+++ src/services/ListagemService.js
@@ -3,14 +3,16 @@ import CarrosApiService from "./CarrosApiService";
 export default class ListagemService extends CarrosApiService {
 
     async carregarCards(page = 0, sort='preco,desc', filtro = null) {
-        if(filtro !== null) {
-            let urlFiltroMarca = `/carros/filtro/marca/${filtro}?page=${page}&sort=${sort}`;
-            let urlFiltroAno   = `/carros/filtro/ano/${filtro}?page=${page}&sort=${sort}`;
+        if(filtro !== null && filtro !== '') {
+            let urlFiltroMarca  =  `/carros/filtro/marca/${filtro}?page=${page}&sort=${sort}`;
+            let urlFiltroAno    =    `/carros/filtro/ano/${filtro}?page=${page}&sort=${sort}`;
+            let urlFiltroModelo = `/carros/filtro/modelo/${filtro}?page=${page}&sort=${sort}`;
 
             let marcas = ['BMW', 'NISSAN', 'AUDI', 'CHERY', 'CHEVROLET', 'CITROEN', 'DODGE'];
 
-            if(marcas.includes(filtro.toUpperCase()))    return await this.get(urlFiltroMarca);
-            else if(typeof(Number(filtro)) === 'number') return await this.get(urlFiltroAno);
+            if(marcas.includes(filtro.toUpperCase())) return await  this.get(urlFiltroMarca);
+            else if(Number.isInteger(Number(filtro))) return await    this.get(urlFiltroAno);
+            else if(typeof(filtro) === 'string')      return await this.get(`/carros/filtro/modelo/${filtro}?page=${page}&sort=${sort}`);
         }
         let url = `/carros?size=8&page=${page}&sort=${sort}`;
         return await this.get(url);
